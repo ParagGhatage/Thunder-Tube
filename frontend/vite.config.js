2f15a85f82328cd1ec911635de0257ed4414a20d
ParagGhatage <phghatage1@gmail.com>
2024-06-16 12:23:12 +0530

    creating build directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// Import PostCSS plugin if needed
+import discardUnused from 'postcss-discard-unused';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +13,19 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  
+  build: {
+    outDir: 'build', // Set the output directory to 'build'
+  },
+  
+  css: {
+    postcss: {
+      plugins: [
+        discardUnused({
+          keyframes: false,
+          // Additional options if needed
+        }),
+      ],
+    },
+  },
+});
