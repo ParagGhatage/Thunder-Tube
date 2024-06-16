e6489454968cb443a63957fbf515c73ab8ae2d12
ParagGhatage <phghatage1@gmail.com>
2024-06-16 13:17:06 +0530

    tailwind issues resolved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
-// Import PostCSS plugin if needed
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 import discardUnused from 'postcss-discard-unused';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   
   resolve: {
     alias: {
@@ -15,12 +17,15 @@ export default defineConfig({
   },
   
   build: {
-    outDir: 'public', // Set the output directory to 'build'
+    outDir: 'build', // Set the output directory to 'build'
   },
+  publicDir: 'public',
   
   css: {
     postcss: {
       plugins: [
+        tailwindcss,
+        autoprefixer,
         discardUnused({
           keyframes: false,
           // Additional options if needed
