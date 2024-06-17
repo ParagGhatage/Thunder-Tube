5928b58e7415415713588097c63d8a270161f3b7
ParagGhatage <phghatage1@gmail.com>
2024-06-17 16:25:25 +0530

    1

--- Backend/src/middlewears/multer.middlewear.js
+++ Backend/src/middlewears/multer.middlewear.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 // Define storage for Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/public/temp'); // Set destination directory
+    cb(null, './public/temp'); // Set destination directory
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname); // Keep original filename
