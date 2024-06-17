b5d4e9c61ac0b0876beab0ca9827f3151a25ccc5
ParagGhatage <phghatage1@gmail.com>
2024-06-17 16:16:55 +0530

    temp path changes

--- Backend/src/middlewears/multer.middlewear.js
+++ Backend/src/middlewears/multer.middlewear.js
@@ -1,13 +1,9 @@
 import multer from 'multer';
-import path from 'path';
 
-// Define absolute path to temp directory
-const tempDir = path.join(__dirname, 'public', 'temp');
-
-// Multer storage configuration
+// Define storage for Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, tempDir); // Use absolute path
+    cb(null, '.../public/temp'); // Set destination directory
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname); // Keep original filename
@@ -15,4 +11,4 @@ const storage = multer.diskStorage({
 });
 
 // Initialize Multer with the configured storage
-export const upload = multer({ storage: storage });
+export const upload = multer({ storage: storage });// Assuming 'avatar' is the field name
