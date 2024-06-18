7b2e44d5feb6025ec3331d60460c05315e6a5b20
ParagGhatage <phghatage1@gmail.com>
2024-06-18 16:51:00 +0530

    path change

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = ({ isAuthenticated, setIsAuthenticated, cookies}) => {
         </button><br/>
         <br/>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          <Link to="/channelviedeos">My Videos</Link>
+          <Link to="/channelvideos">My Videos</Link>
         </button>
       </p>
     </div>
