f20fa9c76a996228e3403beadf6f1eb951941b64
ParagGhatage <phghatage1@gmail.com>
2024-06-18 10:57:43 +0530

    multer changes

--- Backend/src/middlewears/multer.middlewear.js
+++ Backend/src/middlewears/multer.middlewear.js
@@ -1,9 +1,21 @@
 import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+// Replicate __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const uploadDir = path.join(__dirname, 'public/temp');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Define storage for Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/temp'); // Set destination directory
+    cb(null, uploadDir); // Set destination directory
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname); // Keep original filename
@@ -11,4 +23,4 @@ const storage = multer.diskStorage({
 });
 
 // Initialize Multer with the configured storage
-export const upload = multer({ storage: storage });// Assuming 'avatar' is the field name
+const upload = multer({ storage: storage });
