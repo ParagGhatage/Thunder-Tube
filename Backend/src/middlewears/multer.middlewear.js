f371582b64eba97f9c7d1aaffa5fdae497c23658
ParagGhatage <phghatage1@gmail.com>
2024-06-18 11:21:14 +0530

    8

--- Backend/src/middlewears/multer.middlewear.js
+++ Backend/src/middlewears/multer.middlewear.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const uploadDir = path.join(__dirname, '../../tmp');
+const uploadDir = path.join(__dirname, '/tmp');
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
